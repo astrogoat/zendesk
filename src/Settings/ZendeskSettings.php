a9d6f70034085d0d72acf85f69520d5152c0554b
Kofi Romain Amoussou <karsoft92@gmail.com>
2022-06-30 17:53:20 +0000

    additional fields for customization

--- src/Settings/ZendeskSettings.php
+++ src/Settings/ZendeskSettings.php
@@ -7,10 +7,39 @@ use Helix\Lego\Settings\AppSettings;
 class ZendeskSettings extends AppSettings
 {
     public string $app_id;
+    public string $widget_background_color_hex;
+    public string $widget_text_color_hex;
+    public string $widget_offset_left;
+    public string $contact_options;
+    public string $chat_top_bar_background_color_hex;
+    public string $chat_top_bar_text_color_hex;
+    public string $chat_background_color_hex;
+    public string $enable_departments;
+//    public string $departments_enabled;
 
     protected array $rules = [
         'app_id' => ['required'],
+        'widget_background_color_hex' => ['nullable'],
+        'widget_text_color_hex' => ['nullable'],
+        'widget_offset_left'=>['nullable'],
+        'contact_options'=>['required'],
+        'chat_top_bar_background_color_hex' => ['nullable'],
+        'chat_top_bar_text_color_hex' => ['nullable'],
+        'chat_background_color_hex' => ['nullable'],
+        'enable_departments' => ['nullable'],
+//        'departments_enabled' => ['nullable'],
+
     ];
+
+    protected function help(){
+        return [
+            'widget_offset_left'=>'The value should be in px i.e 100px , this represents how far left you want the popup',
+            'contact_options'=> 'The value is either true or false. this will enable a contact option',
+            'enable_departments'=>'The value is either true or false. This will enable you choose departments or disable this feature',
+//            'departments_enabled'=>'This field should contain name of departments to enable seperated by a ,'
+
+        ];
+    }
 
     public function description(): string
     {
