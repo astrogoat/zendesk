a55c32c51943bf76a21fdaeb2dd3d9a183598341
Kofi Romain Amoussou <karsoft92@gmail.com>
2022-07-04 16:17:42 +0000

    group settings and add regex validation

--- src/Settings/ZendeskSettings.php
+++ src/Settings/ZendeskSettings.php
@@ -7,41 +7,64 @@ use Helix\Lego\Settings\AppSettings;
 class ZendeskSettings extends AppSettings
 {
     public string $app_id;
-    public string $widget_background_color;
+    public string $theme_color;
     public string $widget_text_color;
     public string $widget_offset_left;
     public bool $contact_options;
     public string $chat_top_bar_background_color;
     public string $chat_top_bar_text_color;
-    public string $chat_background_color;
-    public bool $enable_departments;
     public string $department;
 
     protected array $rules = [
         'app_id' => ['required'],
-        'widget_background_color' => ['nullable'],
-        'widget_text_color' => ['nullable'],
-        'widget_offset_left'=>['nullable'],
-        'contact_options'=>['required'],
-        'chat_top_bar_background_color' => ['nullable'],
-        'chat_top_bar_text_color' => ['nullable'],
-        'chat_background_color' => ['nullable'],
-        'enable_departments' => ['nullable'],
+        'theme_color' => ['nullable', 'regex:/^(#(?:[0-9a-f]{2}){2,4}|#[0-9a-f]{3}|(?:rgba?|hsla?)\((?:\d+%?(?:deg|rad|grad|turn)?(?:,|\s)+){2,3}[\s\/]*[\d\.]+%?\))$/i'],
+        'widget_text_color' => ['nullable', 'regex:/^(#(?:[0-9a-f]{2}){2,4}|#[0-9a-f]{3}|(?:rgba?|hsla?)\((?:\d+%?(?:deg|rad|grad|turn)?(?:,|\s)+){2,3}[\s\/]*[\d\.]+%?\))$/i'],
+        'widget_offset_left' => ['nullable'],
+        'contact_options' => ['required'],
+        'chat_top_bar_background_color' => ['nullable', 'regex:/^(#(?:[0-9a-f]{2}){2,4}|#[0-9a-f]{3}|(?:rgba?|hsla?)\((?:\d+%?(?:deg|rad|grad|turn)?(?:,|\s)+){2,3}[\s\/]*[\d\.]+%?\))$/i'],
+        'chat_top_bar_text_color' => ['nullable', 'regex:/^(#(?:[0-9a-f]{2}){2,4}|#[0-9a-f]{3}|(?:rgba?|hsla?)\((?:\d+%?(?:deg|rad|grad|turn)?(?:,|\s)+){2,3}[\s\/]*[\d\.]+%?\))$/i'],
         'department' => ['nullable'],
     ];
-
-    protected function help(){
-        return [
-            'widget_offset_left'=>'The value should be in px i.e 100px , this represents how far left you want the popup',
-            'contact_options'=> 'The value is either true or false. this will enable a contact option',
-            'enable_departments'=>'The value is either true or false. This will enable you choose departments or disable this feature',
-            'department'=>'This field should contain name of departments to enable seperated by a comma (,) and lowercase only'
-
-        ];
-    }
 
     public function description(): string
     {
         return 'Zendesk Support is a beautifully simple system for tracking, prioritizing, and solving customer support tickets.';
     }
+
+    public function sections()
+    {
+        return [
+            [
+                'title' => 'Key',
+                'properties' => 'app_id',
+            ],
+            [
+                'title' => 'Widget Customization',
+                'properties' => ['theme_color', 'widget_text_color', 'widget_offset_left']
+            ],
+            [
+                'title' => 'Chat Customization',
+                'properties' => ['chat_top_bar_background_color', 'chat_top_bar_text_color']
+            ],
+            [
+                'title' => 'Extra Settings',
+                'properties' => ['contact_options', 'department']
+            ]
+        ];
+    }
+
+    protected function help()
+    {
+        return [
+            'theme_color' => 'The value should be a hex (#ffffff), this is used as base, determining the overall color scheme of the widget.',
+            'widget_text_color' => 'The value should be a hex (#ffffff), sets the color of the widget text.',
+            'widget_offset_left' => 'The value should be in px i.e 100px , this represents how far left you want the popup.',
+            'chat_top_bar_background_color' => 'The value should be a hex (#ffffff), changes the background of the header when chat is opened.',
+            'chat_top_bar_text_color' => 'The value should be a hex (#ffffff), changes the text color of the launcher.',
+            'contact_options' => 'This will enable a contact option.',
+            'department' => 'This field should contain name of departments to enable seperated by a comma (hr,finance) and lowercase only.'
+        ];
+    }
+
+
 }
