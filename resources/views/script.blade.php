23c31d09045c16409c5571e4dc8e61d5878582b9
Kofi Romain Amoussou <karsoft92@gmail.com>
2022-07-05 10:00:39 +0000

    update tracked files

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,5 +1,36 @@
 @if(Astrogoat\Zendesk\Settings\ZendeskSettings::isEnabled())
+
+
     <!-- Start of helixsleep Zendesk Widget script -->
-    <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key={{ settings(Astrogoat\Zendesk\Settings\ZendeskSettings::class, 'app_id') }}"></script>
+    <script id="ze-snippet"
+            src="https://static.zdassets.com/ekr/snippet.js?key={{ settings(Astrogoat\Zendesk\Settings\ZendeskSettings::class, 'app_id') }}"></script>
+
+    <script type="text/javascript">
+        const dep = '<?php echo json_encode(explode(',',settings(Astrogoat\Zendesk\Settings\ZendeskSettings::class, "department"))) ?>'
+
+        const departments = JSON.parse(dep)
+
+        window.zESettings = {
+            webWidget: {
+                color: {
+                    theme: '{{ settings(Astrogoat\Zendesk\Settings\ZendeskSettings::class, "theme_color") }}',
+                    header: '{{ settings(Astrogoat\Zendesk\Settings\ZendeskSettings::class,"chat_top_bar_background_color")}}',
+                    launcherText: '{{ settings(Astrogoat\Zendesk\Settings\ZendeskSettings::class,"chat_top_bar_text_color")}}',
+                },
+                offset: {
+                    horizontal: '{{ settings(Astrogoat\Zendesk\Settings\ZendeskSettings::class, "widget_offset_left") }}'
+                },
+                contactOptions: {
+                    enabled: {{ settings(Astrogoat\Zendesk\Settings\ZendeskSettings::class, "contact_options")==1? "true" : "false" }}
+                },
+                chat: {
+                    departments: {
+                        enabled: departments, //will hide the Department dropdown
+                        // select: 'Brooklyn Agents' //selects Brooklyn agents department in the background
+                    }
+                }
+            }
+        };
+    </script>
     <!-- End of helixsleep Zendesk Widget script -->
 @endif
